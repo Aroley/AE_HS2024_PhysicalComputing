59ee4ebdb82cedbf9c8adc36625b55a15cba291f
Aroley <47256141+Aroley@users.noreply.github.com>
2025-01-21 12:15:10 +0100

    Timing restrictions working Serial

--- logic.cpp
+++ logic.cpp
@@ -1,5 +1,4 @@
 // https://chariscat.wordpress.com/2020/12/01/adding-additional-analog-inputs-in-arduino-cd4051be-multiplexing-chip/
-
 // https://www.ti.com/lit/ds/symlink/cd4051b.pdf?HQS=dis-dk-null-digikeymode-dsf-pf-null-wwe&ts=1737097661544&ref_url=https%253A%252F%252Fwww.ti.com%252Fgeneral%252Fdocs%252Fsuppproductinfo.tsp%253FdistId%253D10%2526gotoUrl%253Dhttps%253A%252F%252Fwww.ti.com%252Flit%252Fgpn%252Fcd4051b
 
 #include "USB.h"
@@ -11,52 +10,26 @@
 #define readPinD 5  
 #define pinS2 4     
 #define pinS1 2     
-#define pinS0 1     
+#define pinS0 1 
 
-#define KEY_BACKSPACE 0xB2 
-#define KEY_SPACE 0x2C 
-#define KEY_1 0x1E 
-#define KEY_2 0x1F 
-#define KEY_DOT 0x37 
-#define KEY_SLASH 0x38 
-#define KEY_LEFT_SHIFT 0xE1 
 
-#define KEY_A 0x04
-#define KEY_B 0x05
-#define KEY_C 0x06
-#define KEY_D 0x07
-#define KEY_E 0x08
-#define KEY_F 0x09
-#define KEY_G 0x0A
-#define KEY_H 0x0B
-#define KEY_I 0x0C
-#define KEY_J 0x0D
-#define KEY_K 0x0E
-#define KEY_L 0x0F
-#define KEY_M 0x10
-#define KEY_N 0x11
-#define KEY_O 0x12
-#define KEY_P 0x13
-#define KEY_Q 0x14
-#define KEY_R 0x15
-#define KEY_S 0x16
-#define KEY_T 0x17
-#define KEY_U 0x18
-#define KEY_V 0x19
-#define KEY_W 0x1A
-#define KEY_X 0x1B
-#define KEY_Y 0x1C
-#define KEY_Z 0x1D
 
 USBHIDKeyboard Keyboard;
 
-// Arrays containing 8 keycodes each:
-const uint8_t chipArrayA[] = {KEY_BACKSPACE, KEY_1, KEY_2, KEY_SLASH, KEY_DOT, KEY_SPACE, KEY_A, KEY_B}; // KEY_1, KEY_2, KEY_SLASH need LEFTSHIFT pressed
-const uint8_t chipArrayB[] = {KEY_C, KEY_D, KEY_E, KEY_F, KEY_G, KEY_H, KEY_I, KEY_J};
-const uint8_t chipArrayC[] = {KEY_K, KEY_L, KEY_M, KEY_N, KEY_O, KEY_P, KEY_Q, KEY_R};
-const uint8_t chipArrayD[] = {KEY_S, KEY_T, KEY_U, KEY_V, KEY_W, KEY_X, KEY_Y, KEY_Z};
 
-unsigned uint8_t currentKey;
+// Arrays containing keys
+char chipArrayA[] = {'backspace','!', '?', ' ', '.', '@', 'A', 'B'};
+char chipArrayB[] = {'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+char chipArrayC[] = {'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'};
+char chipArrayD[] = {'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+char currentKey;
+
+const int timeThreshold = 24;
+int currentTimeA = 0;
+int currentTimeB = 0;
+int currentTimeC = 0;
+int currentTimeD = 0;
 
 
 void setup() {
@@ -71,62 +44,119 @@ void setup() {
   pinMode(readPinC, INPUT_PULLUP);
   pinMode(readPinD, INPUT_PULLUP);
 
-  Serial.println("Setup done");
+  Keyboard.begin();
+
 }
 
 void loop() {
 
+
   // Chip A reading
    int chipA = readChip(readPinA);
    if (chipA!=0) {
-    currentKey = chipArrayA[chipA]; 
-    /*if (chipA==1 || chipA==2 || chipA==3) {
-    Keyboard.press(KEY_LEFT_SHIFT);
-    Keyboard.press(currentKey);
+    currentKey = chipArrayA[chipA-1]; 
+    currentTimeA++;
+    Serial.print(currentKey);
+    Serial.println(currentTimeA);
+    
+    
+    if(currentTimeA >= timeThreshold){
+      currentTimeA = 0;
+      
+    if (chipA==1) {
+    Keyboard.press(KEY_BACKSPACE);
+    delay(100);
     Keyboard.releaseAll();
-    }*/
+    }else{
+    Keyboard.print(currentKey);
+    Serial.println(currentKey);
+    Serial.println(chipA);
+    }
+    }
+   
 
-    Serial.print("chipA letter : ");
+    /*Serial.print("chipA pin : ");
     Serial.println(chipA);
     Serial.print("chipA key : ");
-    Serial.println(currentKey);
+    Serial.println(currentKey);*/
+   } else{
+    currentTimeA = 0;
    }
-   
-  
 
-  // Chip B reading
-  int chipB = readChip(readPinB);
-  if (chipB!=0) {
-    currentKey = chipArrayB[chipB]; // Read out array position
-    Serial.print("chipB letter : ");
+   // Chip B reading
+   int chipB = readChip(readPinB);
+   if (chipB!=0) {
+    currentKey = chipArrayB[chipB-1]; 
+    currentTimeB++;
+    Serial.print(currentKey);
+    Serial.println(currentTimeB);
+    
+    if(currentTimeB >= timeThreshold){
+      currentTimeB = 0;
+      
+    Keyboard.print(currentKey);
+    Serial.println(currentKey);
+    Serial.println(chipB);
+
+    }
+       /*Serial.print("chipB pin : ");
     Serial.println(chipB);
     Serial.print("chipB key : ");
-    Serial.println(currentKey);
+    Serial.println(currentKey);*/
+   } else{
+    currentTimeB = 0;
    }
-
+   
   // Chip C reading
-  int chipC = readChip(readPinC);
-  if (chipC!=0) {
-    currentKey = chipArrayC[chipC]; // Read out array position
-    Serial.print("chipC letter : ");
+   int chipC = readChip(readPinC);
+   if (chipC!=0) {
+    currentKey = chipArrayC[chipC-1]; 
+    currentTimeC++;
+    Serial.print(currentKey);
+    Serial.println(currentTimeC);
+    
+    
+    if(currentTimeC >= timeThreshold){
+      currentTimeC = 0;
+      
+    Keyboard.print(currentKey);
+    Serial.println(currentKey);
+    Serial.println(chipC);
+    }
+       /*Serial.print("chipC pin : ");
     Serial.println(chipC);
     Serial.print("chipC key : ");
-    Serial.println(currentKey);
+    Serial.println(currentKey);*/
+   } else{
+    currentTimeC = 0;
    }
-  
 
-
-  // Chip D reading
-  int chipD = readChip(readPinD);
-  if (chipD!=0) {
-    currentKey = chipArrayD[chipD]; // Read out array position
-    Serial.print("chipD letter : ");
+// Chip D reading
+   int chipD = readChip(readPinD);
+   if (chipD!=0) {
+    currentKey = chipArrayD[chipD-1]; 
+    currentTimeD++;
+    Serial.print(currentKey);
+    Serial.println(currentTimeD);
+    
+    
+    if(currentTimeD >= timeThreshold){
+      currentTimeD = 0;
+      
+    Keyboard.print(currentKey);
+    Serial.println(currentKey);
     Serial.println(chipD);
-    Serial.print("chipD key : ");
-    Serial.println(currentKey);
+    }
+       /*Serial.print("chipC pin : ");
+    Serial.println(chipC);
+    Serial.print("chipC key : ");
+    Serial.println(currentKey);*/
+   } else{
+    currentTimeD = 0;
    }
 
-  delay(1600);
+
+  delay(100);
 }
 
 int readChip(int readPin) {
